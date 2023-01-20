ea120378cd351446f1935aacfcb49f5727b72661
Tudor Iovita <iovitatudor21@gmail.com>
2023-01-20 17:54:22 +0200

    Change backend url

--- api/index.js
+++ api/index.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
-const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiVGFsa0Vhcm4iLCJ1cmwiOiJodHRwczovL3RhbGtlYXJuLmFwcCIsInVuaXF1ZUlkIjoxMTEzODIzMTY2NTU1LCJpYXQiOjE2NzQxNDI4ODIsImV4cCI6MTY3NDIyOTI4Mn0.ejdgaN-U7FrFvkpVUWwrDV24m4KWlUFdyw80t06pXEE';
-// const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiTGlrZW1lZGlhIiwidXJsIjoiaHR0cHM6Ly9saWtlbWVkaWEuY29tIiwidW5pcXVlSWQiOjE1MTc0MTc0NzE0MCwiaWF0IjoxNjczNjI1Nzc2LCJleHAiOjE2NzM3MTIxNzZ9.ER1epyJrj1h_It21FVkFP3wrYUzldEN9QnxfbNTW0d0';
-const backendUrl = 'http://127.0.0.1:5000/';
-// const backendUrl = 'https://core.talkearn.app/';
+const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiSW5zdGFudGV4cGVydCIsInVybCI6Imh0dHBzOi8vaW5zdGFudGV4cGVydC5vbmxpbmUvIiwidW5pcXVlSWQiOjExMjYxMTQ1Mzg1MjcsImlhdCI6MTY3NDIyOTk4NSwiZXhwIjoxNjc0MzE2Mzg1fQ.wfryWe8GLiKAaluLDqMd2GqbQ3IGtrE-uxBOyP7Dd_o';
+const backendUrl = 'https://core.talkearn.app/';
 
 const defaultConfig = {
   baseURL: backendUrl,
