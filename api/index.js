40a7ec219d45ad3cb6dd6c5277780682b78e8a49
Tudor Iovita <iovitatudor21@gmail.com>
2023-01-26 16:58:08 +0200

    Change backend url

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiSW5zdGFudGV4cGVydCIsInVybCI6Imh0dHBzOi8vaW5zdGFudGV4cGVydC5vbmxpbmUvIiwidW5pcXVlSWQiOjExMjYxMTQ1Mzg1MjcsImlhdCI6MTY3NDIyOTk4NSwiZXhwIjoxNjc0MzE2Mzg1fQ.wfryWe8GLiKAaluLDqMd2GqbQ3IGtrE-uxBOyP7Dd_o';
-const backendUrl = 'https://core.talkearn.app/';
+const backendUrl = 'https://core.instantexpert.online/';
 
 const defaultConfig = {
   baseURL: backendUrl,
