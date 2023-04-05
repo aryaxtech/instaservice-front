6687d8d23b2cf7218e75a60556c4ad51b12d33bd
Tudor Iovita <iovitatudor21@gmail.com>
2023-04-05 11:54:06 +0300

    Change production server data

--- api/index.js
+++ api/index.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-// const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiSW5zdGFudGV4cGVydCIsInVybCI6Imh0dHBzOi8vaW5zdGFudGV4cGVydC5vbmxpbmUvIiwidW5pcXVlSWQiOjgxMjczNDUzODQyNCwiaWF0IjoxNjc4ODc2NzgwLCJleHAiOjE2Nzg5NjMxODB9.6nvMpMRB_QsaHuGSePWj7EU4KDcRSRki2xkGT7IDFU8';
-const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiSW5zdGFudGV4cGVydCIsInVybCI6Imh0dHBzOi8vaW5zdGFudGV4cGVydC5vbmxpbmUvIiwidW5pcXVlSWQiOjE0NDIwNzcyNjE4OSwiaWF0IjoxNjc1NzAwNzcxLCJleHAiOjE2NzU3ODcxNzF9.zFu7t-3xeVH5iWAK02BKG2tiX65TmDXcf3qMHlqWLAo';
-const backendUrl = 'http://127.0.0.1:5000';
-// const backendUrl = 'https://core.instantexpert.online';
+const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiSW5zdGFudGV4cGVydCIsInVybCI6Imh0dHBzOi8vaW5zdGFudGV4cGVydC5vbmxpbmUvIiwidW5pcXVlSWQiOjgxMjczNDUzODQyNCwiaWF0IjoxNjc4ODc2NzgwLCJleHAiOjE2Nzg5NjMxODB9.6nvMpMRB_QsaHuGSePWj7EU4KDcRSRki2xkGT7IDFU8';
+// const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiSW5zdGFudGV4cGVydCIsInVybCI6Imh0dHBzOi8vaW5zdGFudGV4cGVydC5vbmxpbmUvIiwidW5pcXVlSWQiOjE0NDIwNzcyNjE4OSwiaWF0IjoxNjc1NzAwNzcxLCJleHAiOjE2NzU3ODcxNzF9.zFu7t-3xeVH5iWAK02BKG2tiX65TmDXcf3qMHlqWLAo';
+// const backendUrl = 'http://127.0.0.1:5000';
+const backendUrl = 'https://core.instantexpert.online';
 
 const defaultConfig = {
   baseURL: backendUrl,
--- nuxt.config.js
+++ nuxt.config.js
@@ -79,8 +79,8 @@ export default {
     // module options
     sockets: [{
       name: 'main',
-      // url: 'https://core.instantexpert.online',
-      url: 'http://127.0.0.1:5000',
+      url: 'https://core.instantexpert.online',
+      // url: 'http://127.0.0.1:5000',
     }]
   },
 
